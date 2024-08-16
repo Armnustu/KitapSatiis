0e89ee7cacf9e92330c2e3db2896fd0827bb0ca1
Lenovo <Lenovo@DESKTOP-7GJPC0B>
2024-08-16 15:47:20 +0300

    Add project files.

---
+++ OrnekEticaretsitesi/Areas/Admin/Models/ShopingChart.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrnekEticaretsitesi.Areas.Admin.Models
+{
+    public class ShopingChart
+    {
+        public ShopingChart()
+        {
+            Count = 1;
+
+        }
+        [Key]
+        public int ShoppingChartID { get; set; }
+        public string ApplicationUserID { get; set; }
+        [ForeignKey("ApplicationUserID")]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public int ProductID { get; set; }
+
+        [ForeignKey("ProductID")]
+        public Product Product { get; set; }
+        public int Count { get; set; }
+        [NotMapped]
+        public double Price { get; set; }
+    }
+}
